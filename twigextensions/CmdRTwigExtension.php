f861c6a48ff9e81c24690c4b4cac8f2a6ac39cb9
Luke Pearce <luke@ten4design.co.uk>
2017-01-17 15:04:56 +0000

    Add comments

--- twigextensions/CmdRTwigExtension.php
+++ twigextensions/CmdRTwigExtension.php
@@ -42,16 +42,22 @@ class CmdRTwigExtension extends \Twig_Extension
 	public function getBustedAssetFilename($file)
 	{
 		$environmentVariables = craft()->config->get('environmentVariables');
+		// Get the file path using the passed in path and our file system path
 		$filePath = $environmentVariables['fileSystemPath'] . $file;
 		$siteUrl = craft()->getSiteUrl();
 		$fileUrl = $siteUrl . $file;
 
+		// If the file we're trying to bust doesn't exist then we should return the normal filepath
 		if !IOHelper::fileExists($filePath) {
 			return $filePath;
 		}
+
+		// Get the last modified time of the file
 		$lastModified = filemtime($filePath);
+		// Take the first 5 characters of a hashed last modified time
 		$bustString = substr(md5($lastModified), 0, 5);
 
+		// return the original file URL along with a query string set to our 5 character hash
 		return $fileUrl . '?lm=' . $bustString;
 	}
 }
